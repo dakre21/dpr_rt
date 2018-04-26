81345f6fd6813863a96ada3b61321c081d7d4543
David Akre <dakre21@gmail.com>
2018-04-25 21:05:59 -0600

    Temp commit

--- hw/create_pblocks.tcl
+++ hw/create_pblocks.tcl
@@ -7,7 +7,7 @@
 # Create Blob Merge pblock
 startgroup
 create_pblock pblock_bm
-resize_pblock pblock_bm -add CLOCKREGION_X0Y0:CLOCKREGION_X0Y0
+resize_pblock pblock_bm -add {SLICE_X2Y0:SLICE_X49Y49 DSP48_X0Y0:DSP48_X2Y19 RAMB18_X0Y0:RAMB18_X2Y19 RAMB36_X0Y0:RAMB36_X2Y9}
 add_cells_to_pblock pblock_bm [get_cells [list bm]] -clear_locs
 endgroup
 set_property RESET_AFTER_RECONFIG true [get_pblocks pblock_bm] 
@@ -16,7 +16,7 @@ set_property SNAPPING_MODE ON [get_pblocks pblock_bm]
 # Create DiffEq1 pblock
 startgroup
 create_pblock pblock_dq1
-resize_pblock pblock_dq1 -add {SLICE_X26Y101:SLICE_X49Y149 DSP48_X2Y42:DSP48_X2Y59 RAMB18_X2Y42:RAMB18_X2Y59 RAMB36_X2Y21:RAMB36_X2Y29}
+resize_pblock pblock_dq1 -add {SLICE_X80Y101:SLICE_X113Y149 DSP48_X3Y42:DSP48_X4Y59 RAMB18_X4Y42:RAMB18_X5Y59 RAMB36_X4Y21:RAMB36_X5Y29}
 add_cells_to_pblock pblock_dq1 [get_cells [list dq1]] -clear_locs
 endgroup
 set_property RESET_AFTER_RECONFIG true [get_pblocks pblock_dq1]
@@ -25,8 +25,9 @@ set_property SNAPPING_MODE ON [get_pblocks pblock_dq1]
 # Create Sha pblock
 startgroup
 create_pblock pblock_sha
-resize_pblock pblock_sha -add {SLICE_X26Y51:SLICE_X49Y100 DSP48_X2Y22:DSP48_X2Y39 RAMB18_X2Y22:RAMB18_X2Y39 RAMB36_X2Y11:RAMB36_X2Y19}
+resize_pblock pblock_sha -add {SLICE_X80Y0:SLICE_X113Y50 DSP48_X3Y0:DSP48_X4Y19 RAMB18_X4Y0:RAMB18_X5Y19 RAMB36_X4Y0:RAMB36_X5Y9}
 add_cells_to_pblock pblock_sha [get_cells [list sha]] -clear_locs
 endgroup
 set_property RESET_AFTER_RECONFIG true [get_pblocks pblock_sha]
 set_property SNAPPING_MODE ON [get_pblocks pblock_sha]
+
